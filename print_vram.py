bc43fecdc6d7fd0a7bae1768e1f832f5f2d5c534
Dominykas Plesevičius <dominykasplesevicius@gmail.com>
2024-11-03 16:33:07 +0200

    Update vram code

--- print_vram.py
+++ print_vram.py
@@ -1,12 +1,14 @@
-import nvidia_smi
+# import nvidia_smi
+import torch
 
-nvidia_smi.nvmlInit()
+# nvidia_smi.nvmlInit()
 
-handle = nvidia_smi.nvmlDeviceGetHandleByIndex(0)
+# handle = nvidia_smi.nvmlDeviceGetHandleByIndex(0)
 # card id 0 hardcoded here, there is also a call to get all available card ids, so we could iterate
 
-info = nvidia_smi.nvmlDeviceGetMemoryInfo(handle)
+# info = nvidia_smi.nvmlDeviceGetMemoryInfo(handle)
 
-print("Total memory:", info.total/1e9)
-print("Free memory:", info.free/1e9)
-print("Used memory:", info.used/1e9)
+print("Using", torch.cuda.device_count(), "GPUs")
+# print("Total memory:", info.total/1e9)
+# print("Free memory:", info.free/1e9)
+# print("Used memory:", info.used/1e9)
