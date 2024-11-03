519098588829bd3b4626255ab16df3894f557822
Dominykas Plesevičius <dominykasplesevicius@gmail.com>
2024-11-03 17:52:11 +0200

    Tokenize dataset before passing it in

--- train_mgpt.py
+++ train_mgpt.py
@@ -3,38 +3,36 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 from trl import SFTTrainer
 from transformers import TrainingArguments
 import json
-# import torch
+import torch
 from torch.nn.parallel import DistributedDataParallel as DDP
-# import torch.distributed as dist
-from accelerate import PartialState
+import torch.distributed as dist
 
 def train():
-    # n_gpus = torch.cuda.device_count()
-    # dist.init_process_group()
+    n_gpus = torch.cuda.device_count()
+    dist.init_process_group()
 
     tokenizer = AutoTokenizer.from_pretrained("ai-forever/mGPT")
+    model = AutoModelForCausalLM.from_pretrained("ai-forever/mGPT")
 
-    device_string = PartialState().process_index
-    device_map = {'': device_string}
-
-    model = AutoModelForCausalLM.from_pretrained("ai-forever/mGPT", device_map=device_map)
-
-    # if n_gpus > 1:
-    #     model = DDP(model)
+    if n_gpus > 1:
+        model = DDP(model)
 
     train_dataset = load_from_disk("./output-k-200-cleaned-no-bad-words")
     eval_dataset = load_from_disk("./c4-lithuanian-validation")
 
+    tokenized_train_dataset = tokenizer(train_dataset["text"], padding = "max_length", truncation = True, max_length = 2048)
+    tokenized_eval_dataset = tokenizer(eval_dataset["text"], padding = "max_length", truncation = True, max_length = 2048)
+
     trainer = SFTTrainer(
         model = model,
-        tokenizer = tokenizer,
-        train_dataset = train_dataset,
-        eval_dataset = eval_dataset,
+        # tokenizer = tokenizer,
+        train_dataset = tokenized_train_dataset,
+        eval_dataset = tokenized_eval_dataset,
         dataset_text_field = "text",
         max_seq_length = 2048,
 
         args = TrainingArguments(                             
-            gradient_accumulation_steps = 6,
+            gradient_accumulation_steps = 4,
             gradient_checkpointing = True,
             
             num_train_epochs = 3,
@@ -56,7 +54,7 @@ def train():
 
     trainer_stats = trainer.train()
 
-    # dist.destroy_process_group()
+    dist.destroy_process_group()
 
     with open("output-mgpt-train.json", "w") as file:
         file.write(json.dumps(trainer.state.log_history))
