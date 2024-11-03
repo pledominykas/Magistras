66935c1365b2461593c98fabce7bc081286c958c
Dominykas Plesevičius <dominykasplesevicius@gmail.com>
2024-11-03 18:47:25 +0200

    Revert to single gpu

--- train_mgpt.py
+++ train_mgpt.py
@@ -3,62 +3,43 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 from trl import SFTTrainer
 from transformers import TrainingArguments
 import json
-import torch
-from torch.nn.parallel import DistributedDataParallel as DDP
-import torch.distributed as dist
 
-def train():
-    n_gpus = torch.cuda.device_count()
-    dist.init_process_group()
+tokenizer = AutoTokenizer.from_pretrained("ai-forever/mGPT")
+model = AutoModelForCausalLM.from_pretrained("ai-forever/mGPT")
 
-    tokenizer = AutoTokenizer.from_pretrained("ai-forever/mGPT")
-    model = AutoModelForCausalLM.from_pretrained("ai-forever/mGPT")
+train_dataset = load_from_disk("./output-k-200-cleaned-no-bad-words")
+eval_dataset = load_from_disk("./c4-lithuanian-validation")
 
-    if n_gpus > 1:
-        model = DDP(model)
+trainer = SFTTrainer(
+    model = model,
+    tokenizer = tokenizer,
+    train_dataset = train_dataset,
+    eval_dataset = eval_dataset,
+    dataset_text_field = "text",
+    max_seq_length = 2048,
 
-    train_dataset = load_from_disk("./output-k-200-cleaned-no-bad-words")
-    eval_dataset = load_from_disk("./c4-lithuanian-validation")
+    args = TrainingArguments(                             
+        gradient_accumulation_steps = 4,
+        gradient_checkpointing = True,
+        
+        num_train_epochs = 3,
+        learning_rate = 2e-4,
+        per_device_train_batch_size = 8,
+        per_device_eval_batch_size = 8,
 
-    tokenized_train_dataset = tokenizer(train_dataset["text"], padding = "max_length", truncation = True, max_length = 2048)
-    tokenized_eval_dataset = tokenizer(eval_dataset["text"], padding = "max_length", truncation = True, max_length = 2048)
+        seed = 99,
+        output_dir = "./checkpoints-mgpt",
 
-    trainer = SFTTrainer(
-        model = model,
-        # tokenizer = tokenizer,
-        train_dataset = tokenized_train_dataset,
-        eval_dataset = tokenized_eval_dataset,
-        dataset_text_field = "text",
-        max_seq_length = 2048,
+        save_strategy = "steps",
+        eval_strategy = "steps",
 
-        args = TrainingArguments(                             
-            gradient_accumulation_steps = 4,
-            gradient_checkpointing = True,
-            
-            num_train_epochs = 3,
-            learning_rate = 2e-4,
-            per_device_train_batch_size = 8,
-            per_device_eval_batch_size = 8,
+        save_steps = 0.1,
+        eval_steps = 0.1,
+        logging_steps = 0.1
+    ),
+)
 
-            seed = 99,
-            output_dir = "./checkpoints-mgpt",
+trainer_stats = trainer.train()
 
-            save_strategy = "steps",
-            eval_strategy = "steps",
-
-            save_steps = 0.1,
-            eval_steps = 0.1,
-            logging_steps = 0.1
-        ),
-    )
-
-    trainer_stats = trainer.train()
-
-    dist.destroy_process_group()
-
-    with open("output-mgpt-train.json", "w") as file:
-        file.write(json.dumps(trainer.state.log_history))
-
-
-if __name__ == "__main__":
-    train()
+with open("output-mgpt-train.json", "w") as file:
+    file.write(json.dumps(trainer.state.log_history))
