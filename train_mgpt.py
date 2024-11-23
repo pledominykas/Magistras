b78193b873a26a411ae8f33d38d2ce74fe4dd045
Dominykas Plesevičius <dominykasplesevicius@gmail.com>
2024-11-23 15:31:00 +0200

    Speed up training

--- train_mgpt.py
+++ train_mgpt.py
@@ -1,45 +1,56 @@
-from datasets import load_from_disk
+from datasets import load_dataset
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from trl import SFTTrainer
 from transformers import TrainingArguments
-import json
+
 
 tokenizer = AutoTokenizer.from_pretrained("ai-forever/mGPT")
 model = AutoModelForCausalLM.from_pretrained("ai-forever/mGPT")
 
-train_dataset = load_from_disk("./output-k-200-cleaned-no-bad-words")
-eval_dataset = load_from_disk("./c4-lithuanian-validation")
+train_dataset = load_dataset("domce20/c4-lithuanian-final")
+eval_dataset = load_dataset("domce20/c4-lithuanian-validation")
 
-trainer = SFTTrainer(
-    model = model,
-    tokenizer = tokenizer,
-    train_dataset = train_dataset,
-    eval_dataset = eval_dataset,
-    dataset_text_field = "text",
-    max_seq_length = 2048,
+train_dataset = train_dataset["train"]
+eval_dataset = eval_dataset["validation"]
 
-    args = TrainingArguments(                             
-        gradient_accumulation_steps = 4,
+train_dataset = train_dataset.take(10000)
+eval_dataset = eval_dataset.take(1000)
+
+training_args = TrainingArguments(
+        optim = "adamw_bnb_8bit",
+        fp16=True,
+        tf32=True,
         gradient_checkpointing = True,
-        
-        num_train_epochs = 3,
+
+        num_train_epochs = 1,
         learning_rate = 2e-4,
-        per_device_train_batch_size = 8,
+        per_device_train_batch_size = 2,
         per_device_eval_batch_size = 8,
+        gradient_accumulation_steps = 8,
 
         seed = 99,
-        output_dir = "./checkpoints-mgpt",
+        output_dir = "./checkpoints",
 
         save_strategy = "steps",
         eval_strategy = "steps",
 
         save_steps = 0.1,
         eval_steps = 0.1,
-        logging_steps = 0.1
-    ),
+        logging_steps = 0.1,
+        load_best_model_at_end = True,
+    )
+
+trainer = SFTTrainer(
+    model = model,
+    tokenizer = tokenizer,
+
+    train_dataset = train_dataset,
+    eval_dataset = eval_dataset,
+    dataset_text_field = "text",
+
+    max_seq_length = 2048,
+
+    args = training_args,
 )
 
 trainer_stats = trainer.train()
-
-with open("output-mgpt-train.json", "w") as file:
-    file.write(json.dumps(trainer.state.log_history))
