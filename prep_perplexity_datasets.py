fa46f215599e11f38706d4138de666bb95eb4258
Dominykas Plesevičius <dominykasplesevicius@gmail.com>
2024-11-24 17:49:13 +0200

    Add perplexity dataset scripts

---
+++ prep_perplexity_datasets.py
@@ -0,0 +1,22 @@
+from datasets import load_dataset
+
+dataset_wiki = load_dataset("wikimedia/wikipedia", "20231101.lt")
+dataset_lrt = load_dataset("allenai/c4", "lt")
+
+dataset_wiki = dataset_wiki["train"]
+dataset_lrt = dataset_lrt["train"]
+
+dataset_lrt = dataset_lrt.filter(lambda entry: entry["url"].startswith("https://www.lrt.lt/"))
+
+text_file_wiki = 'lithuanian_lrt.txt'
+text_file_lrt = 'lithuanian_wiki.txt'
+
+with open(text_file_wiki, 'w', encoding='utf-8') as f:
+    for example in dataset_wiki:
+        text = example['text'].replace('\n', ' ')
+        f.write(text + '\n')
+
+with open(text_file_lrt, 'w', encoding='utf-8') as f:
+    for example in dataset_lrt:
+        text = example['text'].replace('\n', ' ')
+        f.write(text + '\n')
