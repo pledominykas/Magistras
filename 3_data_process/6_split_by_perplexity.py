0bf27bc7abae91d25b3ef804a1c5baee7d4f8d76
Dominykas Plesevičius <dominykasplesevicius@gmail.com>
2024-11-25 21:00:31 +0200

    Perplexity training split unfinished

--- 3_data_process/6_split_by_perplexity.py
+++ 3_data_process/6_split_by_perplexity.py
@@ -4,17 +4,19 @@ dataset_validation = load_dataset("allenai/c4", "lt", split="validation")["valid
 dataset = load_from_disk("./datasets/c4-lt-filtered-5-short-entries")
 df = dataset.to_pandas()
 
-treshold_1 = df["perplexity_wiki"].quantile(0.01)
-treshold_5 = df["perplexity_wiki"].quantile(0.05)
+one_percent = int(len(df) * 0.01)
+five_percent = int(len(df) * 0.05)
+treshold_25 = df["perplexity_wiki"].quantile(0.25)
+treshold_50 = df["perplexity_wiki"].quantile(0.5)
 
 def split_by_perplexity(entry):
-    if entry["perplexity_wiki"] < treshold_1:
+    if entry["perplexity_wiki"] < treshold_25:
         return {
-            "perplexity_quantile": "1"
+            "perplexity_quantile": "25"
         }
-    elif entry["perplexity_wiki"] < treshold_5:
+    elif entry["perplexity_wiki"] < treshold_50:
         return {
-            "perplexity_quantile": "5"
+            "perplexity_quantile": "50"
         }
     else:
         return {
@@ -23,16 +25,16 @@ def split_by_perplexity(entry):
     
 dataset = dataset.map(split_by_perplexity)
 
-dataset_1 = dataset.filter(lambda x: x["perplexity_quantile"] == "1")
-dataset_5 = dataset.filter(lambda x: x["perplexity_quantile"] == "5")
+dataset_25 = dataset.filter(lambda x: x["perplexity_quantile"] == "25")
+dataset_50 = dataset.filter(lambda x: x["perplexity_quantile"] == "50")
 dataset_100 = dataset.filter(lambda x: x["perplexity_quantile"] == "100")
 
-dataset_1.save_to_disk("./datasets/c4-lt-filtered-6-perplexity-1")
-dataset_5.save_to_disk("./datasets/c4-lt-filtered-6-perplexity-5")
+dataset_25.save_to_disk("./datasets/c4-lt-filtered-6-perplexity-1")
+dataset_50.save_to_disk("./datasets/c4-lt-filtered-6-perplexity-5")
 dataset_100.save_to_disk("./datasets/c4-lt-filtered-6-perplexity-100")
 dataset_validation.save_to_disk("./datasets/c4-lt-filtered-6-perplexity-validation")
 
-dataset_1.cleanup_cache_files()
-dataset_5.cleanup_cache_files()
+dataset_25.cleanup_cache_files()
+dataset_50.cleanup_cache_files()
 dataset_100.cleanup_cache_files()
 dataset_validation.cleanup_cache_files()
