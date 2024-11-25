1d714a9cad55cdfbff25675913252cb772bfc236
Dominykas Plesevičius <dominykasplesevicius@gmail.com>
2024-11-25 19:24:04 +0200

    Add split by quantile

---
+++ 3_data_process/6_split_by_perplexity.py
@@ -0,0 +1,35 @@
+from datasets import load_from_disk
+
+dataset = load_from_disk("./datasets/c4-lt-filtered-5-short-entries")
+df = dataset.to_pandas()
+
+treshold_1 = df["perplexity_wiki"].quantile(0.01)
+treshold_5 = df["perplexity_wiki"].quantile(0.05)
+
+def split_by_perplexity(entry):
+    if entry["perplexity_wiki"] < treshold_1:
+        return {
+            "perplexity_quantile": "1"
+        }
+    elif entry["perplexity_wiki"] < treshold_5:
+        return {
+            "perplexity_quantile": "5"
+        }
+    else:
+        return {
+            "perplexity_quantile": "100"
+        }
+    
+dataset = dataset.map(split_by_perplexity)
+
+dataset_1 = dataset.filter(lambda x: x["perplexity_quantile"] == "1")
+dataset_5 = dataset.filter(lambda x: x["perplexity_quantile"] == "5")
+dataset_100 = dataset.filter(lambda x: x["perplexity_quantile"] == "100")
+
+dataset_1.save_to_disk("./datasets/c4-lt-filtered-6-perplexity-1")
+dataset_5.save_to_disk("./datasets/c4-lt-filtered-6-perplexity-5")
+dataset_100.save_to_disk("./datasets/c4-lt-filtered-6-perplexity-100")
+
+dataset_1.cleanup_cache_files()
+dataset_5.cleanup_cache_files()
+dataset_100.cleanup_cache_files()
