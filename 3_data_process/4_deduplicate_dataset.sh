55d1d53c3726fe4f2d7b5e88701fc35bb505e0b5
Dominykas Plesevičius <dominykasplesevicius@gmail.com>
2024-11-25 21:00:11 +0200

    Update deduplication command

--- 3_data_process/4_deduplicate_dataset.sh
+++ 3_data_process/4_deduplicate_dataset.sh
@@ -1,1 +1,7 @@
-python -m text_dedup.suffix_array --path "allenai/c4" --name "lt" --split "train" --cache_dir "./cache" --output "output" --column "text" --google_repo_path "deduplicate-text-datasets"
+#!/bin/bash
+#SBATCH -p main
+#SBATCH -n36
+
+. "$HOME/.cargo/env"
+
+python3 -m text_dedup.suffix_array --path "./datasets/c4-lt-filtered-3-bad-words" --local --k 500 --cache_dir "./cache" --output "./datasets/c4-lt-filtered-4-deduplicated" --column "text" --google_repo_path "Magistras/deduplicate-image/deduplicate-text-datasets"
