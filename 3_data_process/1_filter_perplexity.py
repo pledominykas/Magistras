e3851581f755866bf5b15d862e14d21677d018f5
Dominykas Plesevičius <dominykasplesevicius@gmail.com>
2024-11-25 20:29:51 +0200

    Update filtering logic

--- 3_data_process/1_filter_perplexity.py
+++ 3_data_process/1_filter_perplexity.py
@@ -1,14 +1,15 @@
 from datasets import load_dataset
 from multiprocessing import cpu_count
+import pandas as pd
 
 def filter_by_perplexity(entry, threshold):
     return entry["perplexity_wiki"] < threshold
 
 if __name__ == "__main__":
     dataset = load_dataset("domce20/c4-lithuanian-enhanced")
-    df = dataset.to_pandas()
+    df = pd.DataFrame(dataset["train"]["perplexity_wiki"], columns=["perplexity"])
 
-    threshold = df["perplexity_wiki"].quantile(0.75)
+    threshold = df["perplexity"].quantile(0.75)
 
     results = dataset.filter(filter_by_perplexity, threshold=threshold, num_proc=cpu_count())
 
