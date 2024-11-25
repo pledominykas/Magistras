3f22955dd89cc6213efc9e34a9fbb1693add963c
Dominykas Plesevičius <dominykasplesevicius@gmail.com>
2024-11-25 21:27:25 +0200

    Add split

--- 3_data_process/4_deduplicate_dataset.sh
+++ 3_data_process/4_deduplicate_dataset.sh
@@ -4,4 +4,4 @@
 
 . "$HOME/.cargo/env"
 
-python3 -m text_dedup.suffix_array --path "./datasets/c4-lt-filtered-3-bad-words" --local --k 500 --cache_dir "./cache" --output "./datasets/c4-lt-filtered-4-deduplicated" --column "text" --google_repo_path "../deduplicate-text-datasets"
+python3 -m text_dedup.suffix_array --path "./datasets/c4-lt-filtered-3-bad-words" --local --split "train" --k 500 --cache_dir "./cache" --output "./datasets/c4-lt-filtered-4-deduplicated" --column "text" --google_repo_path "../deduplicate-text-datasets"
