61b1f93fc538dc3f408a04136b8cb3fab20267e7
Dominykas Plesevičius <dominykasplesevicius@gmail.com>
2024-11-25 19:27:19 +0200

    Save validation dataset locally

--- 3_data_process/6_split_by_perplexity.py
+++ 3_data_process/6_split_by_perplexity.py
@@ -1,5 +1,6 @@
-from datasets import load_from_disk
+from datasets import load_from_disk, load_dataset
 
+dataset_validation = load_dataset("allenai/c4", "lt", split="validation")["validation"]
 dataset = load_from_disk("./datasets/c4-lt-filtered-5-short-entries")
 df = dataset.to_pandas()
 
@@ -29,7 +30,9 @@ dataset_100 = dataset.filter(lambda x: x["perplexity_quantile"] == "100")
 dataset_1.save_to_disk("./datasets/c4-lt-filtered-6-perplexity-1")
 dataset_5.save_to_disk("./datasets/c4-lt-filtered-6-perplexity-5")
 dataset_100.save_to_disk("./datasets/c4-lt-filtered-6-perplexity-100")
+dataset_validation.save_to_disk("./datasets/c4-lt-filtered-6-perplexity-validation")
 
 dataset_1.cleanup_cache_files()
 dataset_5.cleanup_cache_files()
 dataset_100.cleanup_cache_files()
+dataset_validation.cleanup_cache_files()
