103b7ff99449e1862ae2a5afb69ae35df59599c8
Dominykas Plesevičius <dominykasplesevicius@gmail.com>
2024-12-07 23:14:07 +0200

    Add train tokenizer script

---
+++ train_tokenizer.py
@@ -0,0 +1,26 @@
+import os
+
+os.environ["TOKENIZERS_PARALLELISM"] = "1"
+
+from transformers import AutoTokenizer
+from datasets import load_dataset
+
+raw_datasets = load_dataset("allenai/c4", "lt")
+
+raw_datasets = raw_datasets.remove_columns(["timestamp", "url"])
+
+def get_training_corpus():
+    return (
+        raw_datasets["train"][i : i + 1000]["text"]
+        for i in range(0, len(raw_datasets["train"]), 1000)
+    )
+
+training_corpus = get_training_corpus()
+
+old_tokenizer = AutoTokenizer.from_pretrained("ai-forever/mGPT")
+
+print(os.environ["TOKENIZERS_PARALLELISM"])
+
+tokenizer = old_tokenizer.train_new_from_iterator(training_corpus, 100000)
+
+tokenizer.save_pretrained("mGPT-lithuanian-tokenizer")
