16e3b6f7da00171344715d88925b37276584b54c
Dominykas Plesevičius <dominykasplesevicius@gmail.com>
2024-12-12 22:48:34 +0200

    Change back optimizer

--- 4_train_models/mgpt_lt/1_train_mgpt_lt_512.py
+++ 4_train_models/mgpt_lt/1_train_mgpt_lt_512.py
@@ -22,7 +22,7 @@ eval_dataset = load_from_disk("./datasets/c4-lt-filtered-6-perplexity-validation
 training_args = TrainingArguments(
         resume_from_checkpoint=checkpoint_path,
 
-        optim = "adamw_torch",
+        optim = "adamw_bnb_8bit",
         # fp16 = True,
         gradient_checkpointing = True,
 
