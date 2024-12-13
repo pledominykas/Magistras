6850e77ee74884ffd524d33a83721abb9c5b38aa
Dominykas Plesevičius <dominykasplesevicius@gmail.com>
2024-12-13 12:11:21 +0200

    Change batch sizes

--- 4_train_models/mgpt_lt/1_train_mgpt_lt_512.py
+++ 4_train_models/mgpt_lt/1_train_mgpt_lt_512.py
@@ -27,9 +27,9 @@ training_args = TrainingArguments(
 
         num_train_epochs = 1,
         learning_rate = 2e-4,
-        per_device_train_batch_size = 1,
-        per_device_eval_batch_size = 1,
-        gradient_accumulation_steps = 16,
+        per_device_train_batch_size = 2,
+        per_device_eval_batch_size = 2,
+        gradient_accumulation_steps = 8,
 
         seed = 99,
         output_dir = "./checkpoints-mgpt-lt-512",
